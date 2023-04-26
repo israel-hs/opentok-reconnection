43e82fa60e936095c3fbc512619b3fbffc8c1482
Israel Romero <israelromero@hsadmins-mbp.home>
2023-04-26 13:23:09 +0100

    adding config file with token and sessionId

--- js-sample/web/js/config.js
+++ js-sample/web/js/config.js
@@ -11,9 +11,11 @@
 // See https://tokbox.com/developer/sdks/server/ on how to use and deploy
 // an OpenTok Server SDK.
 
-var apiKey = 'YOUR-API-KEY';
-var sessionId = 'YOUR-SESSION-ID';
-var token = 'YOUR-SESSION-TOKEN';
+var apiKey = "45828062";
+var sessionId =
+  "2_MX40NTgyODA2Mn5-MTY4MjQyOTM3NjI5MX5RVHZJeGNIU0VBSys2NVh6RWttUWdXYjR-UH5-";
+var token =
+  "T1==cGFydG5lcl9pZD00NTgyODA2MiZzaWc9Njg4NmM5ZGMyNTkzNGRmYmZlZDdkMzE0MDFhN2NkMGY4OTVmMjdkYzpzZXNzaW9uX2lkPTJfTVg0ME5UZ3lPREEyTW41LU1UWTRNalF5T1RNM05qSTVNWDVSVkhaSmVHTklVMFZCU3lzMk5WaDZSV3R0VVdkWFlqUi1VSDUtJmNyZWF0ZV90aW1lPTE2ODI0MzA5NDgmbm9uY2U9MC41NjAyMzk1Mzg3MjAzNjM0JnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE2ODI1MTczNDg=";
 
 // Set this to true and signals initiated while reconnecting to a session
 // will be sent when reconnected:
